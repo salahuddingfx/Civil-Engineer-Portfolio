ce7fcfeda632ae3b4e4c70f69c51161b9e7c329b
salahuddingfx <salauddinkaderappy@gmail.com>
2026-04-12 06:35:07 +0600

    Backend: Initialize debug registry for system monitoring

--- backend/debug_registry.js
+++
@@ -1,4 +0,0 @@
-const registry = require("./models/modelRegistry");
-console.log("Registry Keys:", Object.keys(registry));
-console.log("Skills Model:", registry.skills ? "LOADED" : "MISSING");
-process.exit(0);
